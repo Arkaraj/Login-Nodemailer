69938aabdc6f892e42f5fb96b95c5b2393071ec4
Arkaraj <arkaraj2017@gmail.com>
2021-01-07 23:56:11 +0530

    Added MySql DB :rocket:

---
+++ Login.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jan 07, 2021 at 06:23 PM
+-- Server version: 8.0.19
+-- PHP Version: 7.1.23
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `Login`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Users`
+--
+
+CREATE TABLE `Users` (
+  `id` int NOT NULL,
+  `Username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `Users`
+--
+
+INSERT INTO `Users` (`id`, `Username`, `email`, `password`) VALUES
+(1, 'Arkaraj', 'node@test.com', 'test1234');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Users`
+--
+ALTER TABLE `Users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Users`
+--
+ALTER TABLE `Users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const nodemailer = require("nodemailer");
+const mysql = require('mysql');
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+    host: `${process.env.HOST}`,
+    user: `${process.env.USER_DB}`,
+    password: `${process.env.PASSWORD}`,
+    database: `${process.env.DB_NAME}`
+});
+
+connection.connect(err => {
+    if (err) {
+        console.log('Error in connecting...');
+        return;
+    } else
+        console.log('connected as id ' + connection.threadId);
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Values from 1000 to 9999
 let OTP = Math.floor(Math.random() * 9000) + 1000;
@@ -8,14 +29,42 @@ let OTP = Math.floor(Math.random() * 9000) + 1000;
 
 app.use(express.static(path.join(__dirname, 'Public')));
 
-/*app.post('/', (req,res)=>{
+let testAccount = await nodemailer.createTestAccount();
 
-});*/
+// create reusable transporter object using the default SMTP transport
+let transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+        user: `${proces.env.MAIL_USER}`,
+        pass: `${proces.env.MAIL_PASS}`
+    }
+});
+
+// send mail with defined transport object
+let info = await transporter.sendMail({
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: "bar@example.com, baz@example.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+});
+
+console.log("Message sent: %s", info.messageId);
+// Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+// Preview only available when sending through an Ethereal account
+console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+// Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 
 app.get('/home', (req, res) => {
     //res.sendFile('./Public/login.html', { root: __dirname });
     res.send('<h1>This is the Home page</h1>');
 });
+
+/*app.post('/', (req,res)=>{
+
+});*/
 
 
 const port = process.env.PORT || 5000;
